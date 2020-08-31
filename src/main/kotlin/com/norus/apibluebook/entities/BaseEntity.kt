61eac43d186e27b5e639110123e3bea0124fb919
Clayton Fellipe <c.fellipe18@gmail.com>
2020-08-31 19:21:19 -0300

    fix: removing local date time now

--- src/main/kotlin/com/norus/apibluebook/entities/BaseEntity.kt
+++ src/main/kotlin/com/norus/apibluebook/entities/BaseEntity.kt
@@ -6,5 +6,5 @@ import javax.persistence.*
 @MappedSuperclass
 abstract class BaseEntity(
         @Column(name = "created_at") open val createdAt: LocalDateTime,
-        @Column(name = "updated_at") open val updatedAt: LocalDateTime = LocalDateTime.now(),
+        @Column(name = "updated_at") open val updatedAt: LocalDateTime,
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY) open val id: Long? = null)
