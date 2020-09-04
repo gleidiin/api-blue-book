445b0c5bf92b6deeb9e5b215398e02626c32b60b
Clayton Fellipe <c.fellipe18@gmail.com>
2020-09-04 20:44:01 -0300

    fix: tests adjusted

--- src/test/kotlin/com/norus/apibluebook/service/TemplateChallengeTest.kt
+++ src/test/kotlin/com/norus/apibluebook/service/TemplateChallengeTest.kt
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringRunner
 
@@ -20,6 +21,7 @@ import org.springframework.test.context.junit4.SpringRunner
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(initializers = [ContainerConfig.Initializer::class], classes = [HikariContainer::class])
+@ActiveProfiles(profiles = arrayOf("dev"))
 class TemplateChallengeTest {
 
     @Autowired
