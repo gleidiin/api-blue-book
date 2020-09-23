055c230ab2099e9108a38b0d77fb25c1433b60e0
Clayton Fellipe <c.fellipe18@gmail.com>
2020-09-22 21:25:55 -0300

    feature: creating reactive template challenge controller

--- src/main/kotlin/com/norus/apibluebook/controllers/TemplateChallengeController.kt
+++ src/main/kotlin/com/norus/apibluebook/controllers/TemplateChallengeController.kt
@@ -1,5 +1,6 @@
 package com.norus.apibluebook.controllers
 
+import com.norus.apibluebook.controllers.dtos.QuestionDTO
 import com.norus.apibluebook.controllers.dtos.TemplateChallengeDTO
 import com.norus.apibluebook.services.TemplateChallengeService
 import org.springframework.http.HttpStatus
@@ -10,8 +11,25 @@ import java.sql.Time
 import java.time.LocalTime
 
 @Controller
-@RequestMapping("/template-challenger")
+@RequestMapping("/v1/template-challenge")
 data class TemplateChallengeController(val templateChallengeService: TemplateChallengeService) {
 
+    @PostMapping
+    fun create(templateChallenge: TemplateChallengeDTO) = ResponseEntity.status(HttpStatus.CREATED)
+            .body(templateChallengeService.createTemplateChallenge(templateChallenge))
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: Long) = ResponseEntity.status(HttpStatus.OK)
+            .body(templateChallengeService.findTemplateById(id))
+
+    @PutMapping("/{id}")
+    fun edit(@PathVariable id: Long, @RequestBody templateChallengeDTO: TemplateChallengeDTO) = ResponseEntity.status(HttpStatus.OK)
+            .body(templateChallengeService.updateTemplateChallenge(id, templateChallengeDTO))
+
+    @DeleteMapping
+    fun delete(@PathVariable id: Long): ResponseEntity<Any> {
+        templateChallengeService.deleteTemplateChallenge(id)
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
+    }
 
 }
--- src/main/kotlin/com/norus/apibluebook/services/TemplateChallengeService.kt
+++ src/main/kotlin/com/norus/apibluebook/services/TemplateChallengeService.kt
@@ -2,12 +2,37 @@ package com.norus.apibluebook.services
 
 import com.norus.apibluebook.configs.AppError
 import com.norus.apibluebook.configs.AppException
+import com.norus.apibluebook.controllers.dtos.QuestionDTO
 import com.norus.apibluebook.controllers.dtos.TemplateChallengeDTO
 import com.norus.apibluebook.repositories.TemplateChallengeRepository
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
 
 @Service
 data class TemplateChallengeService(val templateChallengeRepository: TemplateChallengeRepository) {
+
+
+    fun createTemplateChallenge(templateChallenge: TemplateChallengeDTO) = templateChallengeRepository
+            .save(templateChallenge.convertToTemplateChallage())
+            .map { TemplateChallengeDTO.Companion::fromTemplateChallenge }
+
+    fun findTemplateById(id: Long) = findTemplate(id)
+            .map(TemplateChallengeDTO.Companion::fromTemplateChallenge)
+
+    fun updateTemplateChallenge(id: Long, templateChallenge: TemplateChallengeDTO) = findTemplate(id)
+            .flatMap {
+                it.name = templateChallenge.name
+                it.description = templateChallenge.description
+                templateChallengeRepository.save(it)
+            }.map { TemplateChallengeDTO.Companion::fromTemplateChallenge }
+
+    fun deleteTemplateChallenge(id: Long) {
+        templateChallengeRepository.deleteById(id)
+    }
+
+    private fun findTemplate(id: Long) = templateChallengeRepository
+            .findById(id)
+            .defaultIfEmpty(throw AppException(AppError.QUESTION_NOT_FOUND))
 
 
 }
