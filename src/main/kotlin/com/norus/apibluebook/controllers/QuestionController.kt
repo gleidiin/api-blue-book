0f38383d87e1005975779c8e73d92e33581d9a9b
Gleidson Braga <gleidiin@gmail.com>
2021-06-14 23:31:12 -0300

    refact(question controller): refectory question controller for use server response

--- src/main/kotlin/com/norus/apibluebook/controllers/QuestionController.kt
+++ src/main/kotlin/com/norus/apibluebook/controllers/QuestionController.kt
@@ -15,36 +15,42 @@ import reactor.core.publisher.Mono
 @RequestMapping("/v1/questions/")
 data class QuestionController(val questionService: QuestionService, val answerService: AnswerService) {
     @GetMapping("")
-    fun getAll() = ResponseEntity.status(HttpStatus.OK)
-            .body(questionService.findAllQuestion())
+    fun getAll() = ServerResponse.ok()
+            .body(questionService.findAllQuestion(), QuestionDTO::class.java)
 
     @GetMapping("{id}")
     fun getById(@PathVariable id: Long) = ServerResponse.ok()
-            .body(questionService.findQuestionById(id), QuestionDTO::class.java)
+        .body(questionService.findQuestionById(id), QuestionDTO::class.java)
         .switchIfEmpty(ServerResponse.notFound().build())
 
     @PostMapping
-    fun create(@RequestBody question: QuestionDTO) = ResponseEntity.status(HttpStatus.CREATED)
-            .body(questionService.saveQuestion(question))
+    fun create(@RequestBody question: QuestionDTO) = ServerResponse.status(HttpStatus.CREATED)
+            .body(questionService.saveQuestion(question), QuestionDTO::class.java)
 
     @PutMapping("{id}")
-    fun edit(@PathVariable id: Long, @RequestBody question: QuestionDTO) = ResponseEntity.status(HttpStatus.OK)
-            .body(questionService.updateQuestion(id, question))
+    fun edit(@PathVariable id: Long, @RequestBody question: QuestionDTO) = ServerResponse.ok()
+            .body(questionService.updateQuestion(id, question), QuestionDTO::class.java)
 
     @DeleteMapping("{id}")
-    fun delete(@PathVariable id: Long): ResponseEntity<Mono<Void>> = ResponseEntity.status(HttpStatus.OK).body(questionService.deleteQuestion(id))
+    fun delete(@PathVariable id: Long): ResponseEntity<Mono<Void>> = ResponseEntity.status(HttpStatus.OK)
+        .body(questionService.deleteQuestion(id))
 
     @PostMapping("{id}/answers")
-    fun createAnswer(@RequestBody answer: AnswerDTO, @PathVariable id: Long) {
-        ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(
-            questionService.findQuestionById(id)
-                .flatMap { answerService.saveAnswer(answer,it) }, AnswerDTO::class.java
-        ).switchIfEmpty(ServerResponse.notFound().build())
-    }
+    fun createAnswer(@RequestBody answer: AnswerDTO, @PathVariable id: Long) = ServerResponse.ok()
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(questionService.findQuestionById(id).flatMap { answerService.saveAnswer(answer,it) }, AnswerDTO::class.java)
+        .switchIfEmpty(ServerResponse.notFound().build())
+
 
     @PutMapping("{id}/answers/{idAnswer}")
-    fun editAnswer(@RequestBody answer: AnswerDTO, @PathVariable id: Long, @PathVariable idAnswer: Long) = "Editing new answer"
+    fun editAnswer(@RequestBody answer: AnswerDTO, @PathVariable id: Long, @PathVariable idAnswer: Long) = ServerResponse.ok()
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(questionService.findQuestionById(id).flatMap { answerService.updateAnswer(idAnswer, answer)}, AnswerDTO::class.java)
+        .switchIfEmpty(ServerResponse.notFound().build())
 
     @DeleteMapping("{id}/answers/{idAnswer}")
-    fun deleteAnswer(@PathVariable id: Long, @PathVariable idAnswer: Long) = "Deleting answer"
+    fun deleteAnswer(@PathVariable id: Long, @PathVariable idAnswer: Long) = ServerResponse.ok()
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(questionService.findQuestionById(id).flatMap { answerService.deleteAnswer(idAnswer)}, AnswerDTO::class.java)
+        .switchIfEmpty(ServerResponse.notFound().build())
 }
